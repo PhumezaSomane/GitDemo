60d6940ee3c2cec6a53fa71515eadfd71c87b5bc
phumeza <msphumeza@gmail.com>
2022-03-02 10:46:10 +0200

    checking for updates

--- src/test/java/Academy/validateTitle.java
+++ src/test/java/Academy/validateTitle.java
@@ -41,6 +41,7 @@ public class validateTitle extends base{
 		Assert.assertEquals(l.getTitle().getText(), "FEATURED COURSES");
 		 log.info("Successfully validated Text message");
 		 System.out.println("Test completed");
+		 System.out.println("updating");
 		 
 		;
 	
